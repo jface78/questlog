49c43d69aefbe793eb7984efb5e5a0241d8a0dc0
Jonathan Face <jface@jonathanface.com>
2014-01-05 02:33:22 -0500

    repurposing beta release

---
+++ beta/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta charset="utf-8">
+<meta name="description" content="Play-by-Post roleplaying games.
+    Online dice and other tools to record and enhance your collaborative
+    storytelling experience.">
+<meta name="robots" content="all">
+<title>QuestLog - Play-by-Post RPGs and Other Distractions</title>
+<link rel="shortcut icon" href="img/favicon.ico">
+
+<link rel="stylesheet" type="text/css" href="css/themes/vader/jquery-ui.min.css">
+<link rel="stylesheet" type="text/css" href="css/themes/vader/jquery.ui.theme.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
+<!--
+<script type="text/javascript"
+        src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+</script>
+
+<script type="text/javascript"
+    src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js">
+</script>-->
+
+
+<script type="text/javascript">
+
+  window.jQuery || document.write('<script type="text/javascript" ' +
+                                   'src="js/plugins/jquery.min.js"><\/script>');
+                                   
+
+  window.jQuery.ui || document.write('<script type="text/javascript" ' +
+                                   'src="js/plugins/jquery-ui.min.js"><\/script>');
+</script>
+
+<script type="text/javascript" src="js/main.js"></script>
+
+</head>
+<body>
+<div id="mainContent">
+<img alt="QuestLog" id="logo" src="img/logo_day.png">
+</div>
+<footer>
+<b>&copy;INFINITY</b> questlog.org. A real lawyer wrote this, seriously.
+</footer>
+</body>
+</html>
