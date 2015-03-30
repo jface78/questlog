d7548084e6d07d0b75d1e393951c0d27a4acd468
Jonathan Face <jface@dictumnet.com>
2015-03-30 07:58:54 -0400

    fixing formatting

--- charlie/api/v1/QuestlogAPI.php
+++ charlie/api/v1/QuestlogAPI.php
@@ -19,225 +19,225 @@ class QuestlogAPI extends API {
     }
   }
     
-    private function verifyKey($key) {
-      if ($key == 1) {
-        return true;
-      } else {
-        return false;
-      }
+  private function verifyKey($key) {
+    if ($key == 1) {
+      return true;
+    } else {
+      return false;
     }
+  }
     
-    protected function login($args) {
+  protected function login($args) {
+    try {
+      $dbh = new PDO('mysql:host=' .DB_HOST . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $query = 'SELECT u.uid,u.login_name,g.group_name FROM users u, groups g
+          WHERE u.login_name=:name
+          AND u.login_hash=:hash
+          AND u.gid=g.gid';
+      $sth = $dbh -> prepare($query);
+      $sth -> execute(array(':name' => $args[0], ':hash' => hashPasswd($args[0], $args[1])));
+      $row = $sth -> fetch();
+
+      if(!$row) {
+        $dbh = null;
+        http_response_code(401);
+        exit();
+      } else {
+        $_SESSION['uid'] = $row['uid'];
+        $_SESSION['login'] = $row['login_name'];
+        $_SESSION['group'] = $row['group_name'];
+        $_SESSION['last_activity'] = time();
+
+        $query = 'SELECT ip, date FROM user_logins WHERE uid=:uid ORDER BY date DESC LIMIT 1';
+        $sth = $dbh -> prepare($query);
+        $sth -> execute(array(':uid' => $row['uid']));
+        $login_data = $sth -> fetch();
+        if (!$login_data) {
+          $login_data['ip'] = $_SERVER['REMOTE_HOST'];
+          $login_data['date'] = time();
+        }
+        $_SESSION["ip"] = $login_data['ip'];
+        $_SESSION["date"] = $login_data['date'];
+        $query = "INSERT INTO user_logins (date, ip) VALUES (now(),:ip)";
+        $sth = $dbh->prepare($query);
+        $sth->execute(array(':ip' => $_SERVER['REMOTE_ADDR']));
+        $dbh = null;
+        $json_array = [];
+        $json_array['user_details'] = [];
+        $json_array['user_details']['name'] = $row['login_name'];
+        $json_array['user_details']['ip'] = $login_data['ip'];
+        $json_array['user_details']['date'] = $login_data['date'];
+        return $json_array;
+      }
+    } catch(PDOException $error) {
+      return 'database_error';
+    }
+  }
+
+  protected function logout() {
+    killSession();
+    return 'logged_out';
+  }
+
+  protected function quests() {
+    if ($this -> method == 'GET') {
       try {
         $dbh = new PDO('mysql:host=' .DB_HOST . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $query = 'SELECT u.uid,u.login_name,g.group_name FROM users u, groups g
-            WHERE u.login_name=:name
-            AND u.login_hash=:hash
-            AND u.gid=g.gid';
+        // Get the user's GM quest.
+        $query = 'SELECT qid,uid,quest_name,timestamp FROM quests WHERE uid=:uid AND status > 0';
         $sth = $dbh -> prepare($query);
-        $sth -> execute(array(':name' => $args[0], ':hash' => hashPasswd($args[0], $args[1])));
-        $row = $sth -> fetch();
-
-        if(!$row) {
-          $dbh = null;
-          http_response_code(401);
-          exit();
-        } else {
-          $_SESSION['uid'] = $row['uid'];
-          $_SESSION['login'] = $row['login_name'];
-          $_SESSION['group'] = $row['group_name'];
-          $_SESSION['last_activity'] = time();
-
-          $query = 'SELECT ip, date FROM user_logins WHERE uid=:uid ORDER BY date DESC LIMIT 1';
-          $sth = $dbh -> prepare($query);
-          $sth -> execute(array(':uid' => $row['uid']));
-          $login_data = $sth -> fetch();
-          if (!$login_data) {
-            $login_data['ip'] = $_SERVER['REMOTE_HOST'];
-            $login_data['date'] = time();
-          }
-          $_SESSION["ip"] = $login_data['ip'];
-          $_SESSION["date"] = $login_data['date'];
-          $query = "INSERT INTO user_logins (date, ip) VALUES (now(),:ip)";
-          $sth = $dbh->prepare($query);
-          $sth->execute(array(':ip' => $_SERVER['REMOTE_ADDR']));
-          $dbh = null;
-          $json_array = [];
-          $json_array['user_details'] = [];
-          $json_array['user_details']['name'] = $row['login_name'];
-          $json_array['user_details']['ip'] = $login_data['ip'];
-          $json_array['user_details']['date'] = $login_data['date'];
-          return $json_array;
+        $sth -> execute(array(':uid' => $_SESSION['uid']));
+        $questsArr = $sth -> fetchAll();
+        $gm_quests = '0';
+        foreach ($questsArr as $quest) {
+          $gm_quests .= ',' . $quest['qid'];
         }
+        $json_array = [];
+        $json_array['quests'] = [];
+        $json_array = generateQuestListings($questsArr, $json_array, 'gmQuests');
+
+        // Get the user's player quests.
+        $query = 'SELECT cid FROM characters WHERE uid=:uid';
+        $sth = $dbh -> prepare($query);
+        $sth -> execute(array(':uid' => $_SESSION['uid']));
+        $results = $sth -> fetchAll();
+        $questsArr = [];
+        $index=0;
+        $player_chars = '0';
+        foreach ($results as $row) {
+          $query = 'SELECT qid FROM quest_members WHERE cid=:cid';
+          $sth = $dbh -> prepare($query);
+          $sth -> execute(array(':cid' => $row['cid']));
+          $player_chars .= ',' . $row['cid'];
+          $player_quests = $sth -> fetchAll();
+          foreach ($player_quests as $quest) {
+            $query = 'SELECT qid,uid,quest_name,timestamp FROM quests WHERE qid=:qid AND status > 0';
+            $sth = $dbh -> prepare($query);
+            $sth -> execute(array(':qid' => $quest['qid']));
+            $questsArr[$index] = $sth -> fetch();
+            $index++;
+          }
+        }
+        $json_array = generateQuestListings($questsArr, $json_array, 'playerQuests');
+
+        // Get all remaining  non-player, non-GM quests.
+        $query = 'SELECT qid FROM quest_members WHERE qid NOT IN (' . $gm_quests .') AND cid NOT IN (' . $player_chars . ')';
+        $sth = $dbh -> prepare($query);
+        $sth -> execute();
+        $non_quests = $sth -> fetchAll();
+        $questsArr = [];
+        $index = 0;
+        foreach ($non_quests as $quest) {
+          $query = 'SELECT qid,uid,quest_name,timestamp FROM quests WHERE qid=:qid AND status > 0';
+          $sth = $dbh -> prepare($query);
+          $sth -> execute(array(':qid' => $quest['qid']));
+          $results = $sth -> fetch(PDO::FETCH_ASSOC);
+          if ($results['quest_name'] != null) {
+            $questsArr[$index] = $results;
+          }
+          $index++;
+        }
+        usort($questsArr, function($a, $b) {
+          return $a['quest_name'] > $b['quest_name'];
+        });
+        $json_array = generateQuestListings($questsArr, $json_array, 'otherQuests');
+        $dbh = null;
+        return $json_array;
       } catch(PDOException $error) {
         return 'database_error';
       }
     }
+  }
 
-    protected function logout() {
-      killSession();
-      return 'logged_out';
-    }
-
-    protected function quests() {
-      if ($this -> method == 'GET') {
-        try {
-          $dbh = new PDO('mysql:host=' .DB_HOST . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
-          // Get the user's GM quest.
-          $query = 'SELECT qid,uid,quest_name,timestamp FROM quests WHERE uid=:uid AND status > 0';
-          $sth = $dbh -> prepare($query);
-          $sth -> execute(array(':uid' => $_SESSION['uid']));
-          $questsArr = $sth -> fetchAll();
-          $gm_quests = '0';
-          foreach ($questsArr as $quest) {
-            $gm_quests .= ',' . $quest['qid'];
-          }
-          $json_array = [];
-          $json_array['quests'] = [];
-          $json_array = generateQuestListings($questsArr, $json_array, 'gmQuests');
-
-          // Get the user's player quests.
-          $query = 'SELECT cid FROM characters WHERE uid=:uid';
-          $sth = $dbh -> prepare($query);
-          $sth -> execute(array(':uid' => $_SESSION['uid']));
-          $results = $sth -> fetchAll();
-          $questsArr = [];
-          $index=0;
-          $player_chars = '0';
-          foreach ($results as $row) {
-            $query = 'SELECT qid FROM quest_members WHERE cid=:cid';
-            $sth = $dbh -> prepare($query);
-            $sth -> execute(array(':cid' => $row['cid']));
-            $player_chars .= ',' . $row['cid'];
-            $player_quests = $sth -> fetchAll();
-            foreach ($player_quests as $quest) {
-              $query = 'SELECT qid,uid,quest_name,timestamp FROM quests WHERE qid=:qid AND status > 0';
-              $sth = $dbh -> prepare($query);
-              $sth -> execute(array(':qid' => $quest['qid']));
-              $questsArr[$index] = $sth -> fetch();
-              $index++;
-            }
-          }
-          $json_array = generateQuestListings($questsArr, $json_array, 'playerQuests');
-
-          // Get all remaining  non-player, non-GM quests.
-          $query = 'SELECT qid FROM quest_members WHERE qid NOT IN (' . $gm_quests .') AND cid NOT IN (' . $player_chars . ')';
-          $sth = $dbh -> prepare($query);
-          $sth -> execute();
-          $non_quests = $sth -> fetchAll();
-          $questsArr = [];
-          $index = 0;
-          foreach ($non_quests as $quest) {
-            $query = 'SELECT qid,uid,quest_name,timestamp FROM quests WHERE qid=:qid AND status > 0';
-            $sth = $dbh -> prepare($query);
-            $sth -> execute(array(':qid' => $quest['qid']));
-            $results = $sth -> fetch(PDO::FETCH_ASSOC);
-            if ($results['quest_name'] != null) {
-              $questsArr[$index] = $results;
-            }
-            $index++;
-          }
-          usort($questsArr, function($a, $b) {
-            return $a['quest_name'] > $b['quest_name'];
-          });
-          $json_array = generateQuestListings($questsArr, $json_array, 'otherQuests');
-          $dbh = null;
-          return $json_array;
-        } catch(PDOException $error) {
-          return 'database_error';
-        }
+  protected function quest($args) {
+    if ($this->method == 'GET') {
+      $postOrder = 'DESC';
+      $limit = null;
+      $page = 1;
+      if (in_array('ORDER', $args)) {
+        $pos = array_search('ORDER', $args) + 1;
+        $postOrder = strtoupper($args[$pos]);
       }
-    }
+      if (in_array('LIMIT', $args)) {
+        $pos = array_search('LIMIT', $args) + 1;
+        $limit = $args[$pos];
+      }
+      if (in_array('PAGE', $args)) {
+        $pos = array_search('PAGE', $args) + 1;
+        $page = $args[$pos];
+      }
 
-    protected function quest($args) {
-      if ($this->method == 'GET') {
-        $postOrder = 'DESC';
-        $limit = null;
-        $page = 1;
-        if (in_array('ORDER', $args)) {
-          $pos = array_search('ORDER', $args) + 1;
-          $postOrder = strtoupper($args[$pos]);
-        }
-        if (in_array('LIMIT', $args)) {
-          $pos = array_search('LIMIT', $args) + 1;
-          $limit = $args[$pos];
-        }
-        if (in_array('PAGE', $args)) {
-          $pos = array_search('PAGE', $args) + 1;
-          $page = $args[$pos];
-        }
+      try {
+        $dbh = new PDO('mysql:host=' .DB_HOST . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
+        $json_array = [];
+        $query = 'SELECT qid,quest_name FROM quests WHERE qid = :questID AND status > 0';
+        $sth = $dbh -> prepare($query);
+        $sth -> execute(array(':questID' => $args[0]));
+        $row = $sth -> fetch();
 
-        try {
-          $dbh = new PDO('mysql:host=' .DB_HOST . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
-          $json_array = [];
-          $query = 'SELECT qid,quest_name FROM quests WHERE qid = :questID AND status > 0';
-          $sth = $dbh -> prepare($query);
-          $sth -> execute(array(':questID' => $args[0]));
-          $row = $sth -> fetch();
-
-          if (!$row) {
-            $dbh = null;
-            return 'null_results';
-          }
-          $json_array['title'] = $row['quest_name'];
-          $json_array['questID'] = $row['qid'];
-          $json_array['currentPage'] = $page;
-          $json_array['order'] = $postOrder;
-
-          if (is_null($limit)) {
-            $json_array['pageCount'] = 1;
-            $json_array['delimiter'] = 0;
-            $startingIndex = 0;
-          } else {
-            $query = 'SELECT COUNT(pid) FROM posts WHERE qid=:questID';
-            $sth = $dbh -> prepare($query);
-            $sth -> execute(array(':questID' => $args[0]));
-            $total = $sth -> fetch();
-            $json_array['pageCount'] = ceil($total['COUNT(pid)']/$limit);
-            $json_array['delimiter'] = $limit;
-            if ($postOrder == 'DESC') {
-              $startingIndex = $limit * ($page-1);
-            } else {
-              $startingIndex = ($limit * $page) - $limit;
-            }
-          }
-          $query = 'SELECT pid,uid,cid,timestamp,post_text FROM posts WHERE qid=:questID ORDER BY timestamp ' . $postOrder;
-          if (!is_null($limit)) {
-            $query .= ' LIMIT ' . $limit . ' OFFSET ' . $startingIndex;
-          }
-          $sth = $dbh -> prepare($query);
-          $sth -> execute(array(':questID' => $args[0]));
-          $results = $sth -> fetchAll();
-          $index = 0;
-          foreach($results as $row) {
-            $json_array['posts'][$index] = array();
-            $json_array['posts'][$index]['id'] = $row['pid'];
-            $json_array['posts'][$index]['timestamp'] = strtotime($row['timestamp']);
-            $json_array['posts'][$index]['text'] = $row['post_text'];
-            $json_array['posts'][$index]['uid'] = $row['uid'];
-            $json_array['posts'][$index]['cid'] = $row['cid'];
-            if ($row['cid'] == '0') {
-              $json_array['posts'][$index]['cid'] = 'GM';
-              $query = 'SELECT login_name FROM users WHERE uid = :userID';
-              $sth = $dbh -> prepare($query);
-              $sth -> execute(array(':userID' => $row['uid']));
-              $user = $sth -> fetch();
-              $json_array['posts'][$index]['postedBy'] = $user['login_name'];
-            } else {
-              $query = 'SELECT char_name FROM characters WHERE cid = :characterID';
-              $sth = $dbh -> prepare($query);
-              $sth -> execute(array(':characterID' => $row['cid']));
-              $character = $sth -> fetch();
-              $json_array['posts'][$index]['postedBy'] = $character['char_name'];
-            }
-            $index++;
-            if ($index == count($results)) {
-            
-            }
-          }
+        if (!$row) {
           $dbh = null;
-          return $json_array;
+          return 'null_results';
+        }
+        $json_array['title'] = $row['quest_name'];
+        $json_array['questID'] = $row['qid'];
+        $json_array['currentPage'] = $page;
+        $json_array['order'] = $postOrder;
+
+        if (is_null($limit)) {
+          $json_array['pageCount'] = 1;
+          $json_array['delimiter'] = 0;
+          $startingIndex = 0;
+        } else {
+          $query = 'SELECT COUNT(pid) FROM posts WHERE qid=:questID';
+          $sth = $dbh -> prepare($query);
+          $sth -> execute(array(':questID' => $args[0]));
+          $total = $sth -> fetch();
+          $json_array['pageCount'] = ceil($total['COUNT(pid)']/$limit);
+          $json_array['delimiter'] = $limit;
+          if ($postOrder == 'DESC') {
+            $startingIndex = $limit * ($page-1);
+          } else {
+            $startingIndex = ($limit * $page) - $limit;
+          }
+        }
+        $query = 'SELECT pid,uid,cid,timestamp,post_text FROM posts WHERE qid=:questID ORDER BY timestamp ' . $postOrder;
+        if (!is_null($limit)) {
+          $query .= ' LIMIT ' . $limit . ' OFFSET ' . $startingIndex;
+        }
+        $sth = $dbh -> prepare($query);
+        $sth -> execute(array(':questID' => $args[0]));
+        $results = $sth -> fetchAll();
+        $index = 0;
+        foreach($results as $row) {
+          $json_array['posts'][$index] = array();
+          $json_array['posts'][$index]['id'] = $row['pid'];
+          $json_array['posts'][$index]['timestamp'] = strtotime($row['timestamp']);
+          $json_array['posts'][$index]['text'] = $row['post_text'];
+          $json_array['posts'][$index]['uid'] = $row['uid'];
+          $json_array['posts'][$index]['cid'] = $row['cid'];
+          if ($row['cid'] == '0') {
+            $json_array['posts'][$index]['cid'] = 'GM';
+            $query = 'SELECT login_name FROM users WHERE uid = :userID';
+            $sth = $dbh -> prepare($query);
+            $sth -> execute(array(':userID' => $row['uid']));
+            $user = $sth -> fetch();
+            $json_array['posts'][$index]['postedBy'] = $user['login_name'];
+          } else {
+            $query = 'SELECT char_name FROM characters WHERE cid = :characterID';
+            $sth = $dbh -> prepare($query);
+            $sth -> execute(array(':characterID' => $row['cid']));
+            $character = $sth -> fetch();
+            $json_array['posts'][$index]['postedBy'] = $character['char_name'];
+          }
+          $index++;
+          if ($index == count($results)) {
+            
+          }
+        }
+        $dbh = null;
+        return $json_array;
       } catch(PDOException $error) {
         return 'database_error';
       }
