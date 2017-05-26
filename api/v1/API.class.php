57826f9368fb2bab95a9b225689faa6ccb8e8d50
Jonathan Face <jface@dictumnet.com>
2017-05-26 10:49:31 -0400

    checking in delta
    Change-Id: Ibfb49ebfaf1ca6e4550f16b861ed2153767af91e

---
+++ api/v1/API.class.php
@@ -0,0 +1,321 @@
+<?php
+abstract class API {
+    /**
+     * Property: method
+     * The HTTP method this request was made in, either GET, POST, PUT or DELETE
+     */
+    protected $method = '';
+    /**
+     * Property: endpoint
+     * The Model requested in the URI. eg: /files
+     */
+    protected $endpoint = '';
+    /**
+     * Property: verb
+     * An optional additional descriptor about the endpoint, used for things that can
+     * not be handled by the basic methods. eg: /files/process
+     */
+    protected $verb = '';
+    /**
+     * Property: args
+     * Any additional URI components after the endpoint and verb have been removed, in our
+     * case, an integer ID for the resource. eg: /<endpoint>/<verb>/<arg0>/<arg1>
+     * or /<endpoint>/<arg0>
+     */
+    protected $args = Array();
+    /**
+     * Property: file
+     * Stores the input of the PUT request
+     */
+     protected $file = Null;
+
+    /**
+     * Constructor: __construct
+     * Allow for CORS, assemble and pre-process the data
+     */
+    public function __construct($request) {
+        header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Methods: *");
+        header("Content-Type: application/json");
+
+        
+        $this->args = explode('/', rtrim($request, '/'));
+        $this->endpoint = array_shift($this->args);
+        if (array_key_exists(0, $this->args) && !is_numeric($this->args[0])) {
+            //$this->verb = array_shift($this->args);
+        }
+
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        if ($this->method == 'POST' && array_key_exists('HTTP_X_HTTP_METHOD', $_SERVER)) {
+            if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'DELETE') {
+              $this->method = 'DELETE';
+            } else if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'PUT') {
+              $this->method = 'PUT';
+            } else {
+              throw new Exception("Unexpected Header");
+            }
+        }
+
+        switch($this->method) {
+          case 'DELETE':
+            $this->request = $this->_cleanInputs($_GET);
+            break;
+          case 'POST':
+              $this->request = $this->_cleanInputs($_POST);
+              break;
+          case 'GET':
+              $this->request = $this->_cleanInputs($_GET);
+              break;
+          case 'PUT':
+            $this->request = $this->_cleanInputs($_GET);
+            $this->file = file_get_contents("php://input");
+            break;
+          default:
+            $this->_response('Invalid Method', 405);
+            break;
+        }
+    }
+
+    public function processAPI() {
+      // Check that the session is active.
+      if ($this -> endpoint != 'login' && $this -> endpoint != 'session' && !checkSession()) {
+        return $this->_response("Not logged in.", 401);
+      }
+      switch($this ->endpoint) {
+        case 'session':
+          if (!checkSession()) {
+            return $this->_response("Not logged in.", 401);
+          } else {
+            $json_array = [];
+            $json_array['user_details'] = [];
+            $json_array['user_details']['name'] = $_SESSION['login'];
+            $json_array['user_details']['id'] = $_SESSION['uid'];
+            $json_array['user_details']['ip'] = $_SESSION['ip'];
+            $json_array['user_details']['date'] = $_SESSION['date'];
+            return json_encode($json_array);
+          }
+          break;
+        case 'login':
+          if (empty($this->args[0]) || empty($this->args[1])) {
+            return $this->_response("Invalid parameter(s)", 400);
+          }
+          break;
+        case 'quest':
+          $this->args = array_map('strtoupper', $this->args);
+          if ($this->method == 'GET') {
+            if (empty($this->args[0]) || !is_numeric($this->args[0])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+          } else if ($this -> method == 'POST') {
+            $this -> args = array_change_key_case($this->request, CASE_UPPER);
+            if (empty($this->args['BACKSTORY']) || empty($this->args['NAME'])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+          } else if ($this -> method == 'DELETE') {
+            if (empty($this->args[0]) || !is_numeric($this->args[0])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+          } else if ($this -> method == 'PUT') {
+            parse_str($this -> file, $this->args);
+            $this -> args = array_change_key_case($this->args, CASE_UPPER);
+            if (empty($this->args['QID']) || !is_numeric($this->args['QID'])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+          }
+          break;
+        case 'users':
+        //Need to figure out a safe way to make user data available publicly
+        //without compromising privacy.
+          //if ($this->request['apiKey'] == 1) {
+            $this->args = array_map('strtoupper', $this->args);
+            if ($this->method == 'GET') {
+              if (isset($this->args) && is_array($this->args) && count($this->args) >= 1) {
+                if (in_array('UID',$this->args)) {
+                  $nextPos = array_search('UID',$this->args)+1;
+                  if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                    return $this->_response("Invalid parameter(s)", 400);
+                  }
+                }
+                if (in_array('QID',$this->args)) {
+                  $nextPos = array_search('QID',$this->args)+1;
+                  if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                    return $this->_response("Invalid parameter(s)", 400);
+                  }
+                }
+                if (in_array('CID',$this->args)) {
+                  $nextPos = array_search('CID',$this->args)+1;
+                  if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                    return $this->_response("Invalid parameter(s)", 400);
+                  }
+                }
+              }
+            } else if ($this -> method == 'PUT') {
+              parse_str($this -> file, $this->args);
+              $this -> args = array_change_key_case($this->args, CASE_UPPER);
+              if (!empty($this->args['EMAIL'])) {
+                if (!filter_var($this->args['EMAIL'], FILTER_VALIDATE_EMAIL)) {
+                  return $this->_response("Invalid parameter(s)", 400);
+                }
+              } else {
+                // this will check to make sure at least one field is being updated, just email for now.
+                return $this->_response("Invalid parameter(s)", 400);
+              }
+            }
+          //} else {
+            //return $this->_response("Forbidden", 403);
+          //}
+          break;
+        case 'posts':
+          if ($this->method == 'GET') {
+            $this->args = array_map('strtoupper', $this->args);
+            if (!in_array('QID',$this->args) && !in_array('PID',$this->args) && !in_array('CID',$this->args) && !in_array('UID',$this->args)) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+            if (in_array('QID',$this->args) && in_array('PID',$this->args)) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+            if (in_array('PID',$this->args) && (in_array('CID',$this->args) || in_array('UID',$this->args))) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+            if (in_array('CID',$this->args) && in_array('UID',$this->args)) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+            if (in_array('LIMIT',$this->args)) {
+              $nextPos = array_search('LIMIT',$this->args)+1;
+              if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                return $this->_response("Invalid parameter(s)", 400);
+              }
+            }
+            if (in_array('ORDER', $this->args)) {
+              $nextPos = array_search('ORDER', $this->args) + 1;
+              if (count($this->args) >= $nextPos+1 && $this->args[$nextPos] != 'ASC' && $this->args[$nextPos] != 'DESC') {
+                return $this->_response("Invalid parameter(s)", 400);
+              }
+            }
+            if (in_array('PAGE', $this->args)) {
+              $nextPos = array_search('PAGE', $this->args) + 1;
+              if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                return $this->_response("Invalid parameter(s)", 400);
+              }
+            }
+            if (in_array('QID',$this->args)) {
+              $nextPos = array_search('QID',$this->args)+1;
+              if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                return $this->_response("Invalid parameter(s)", 400);
+              }
+            }
+            if (in_array('PID',$this->args)) {
+              $nextPos = array_search('PID',$this->args)+1;
+              if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                return $this->_response("Invalid parameter(s)", 400);
+              }
+            }
+            if (in_array('CID',$this->args)) {
+              $nextPos = array_search('CID',$this->args)+1;
+              if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                return $this->_response("Invalid parameter(s)", 400);
+              }
+            }
+            if (in_array('UID',$this->args)) {
+              $nextPos = array_search('UID',$this->args)+1;
+              if (count($this->args) >= $nextPos+1 && !is_numeric($this->args[$nextPos])) {
+                return $this->_response("Invalid parameter(s)", 400);
+              }
+            }
+          } else if ($this->method == 'POST') {
+            $this -> args = array_change_key_case($this->request, CASE_UPPER);
+            if (empty($this->args['QID']) || empty($this->args['BODY'])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+            if (!is_numeric($this->args['QID']) || !is_numeric($this->args['CID'])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+          } else if ($this->method == 'DELETE') {
+            if (!in_array('PID',$this->args)) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+            $nextPos = array_search('PID',$this->args)+1;
+            if (!is_numeric($this->args[$nextPos])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+          } else if ($this -> method == 'PUT') {
+            parse_str($this -> file, $this->args);
+            $this -> args = array_change_key_case($this->args, CASE_UPPER);
+            if (empty($this->args['PID']) || !is_numeric($this->args['PID'])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+            if (empty($this->args['CID'])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+            if (empty($this->args['BODY'])) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+          }
+          break;
+        case 'search':
+          $this->args = array_map('strtoupper', $this->args);
+          if (!in_array('PLAYERS',$this->args)) {
+            return $this->_response("Invalid parameter(s)", 400);
+          } else {
+            $nextPos = array_search('PLAYERS',$this->args)+1;
+            if (strlen($this->args[$nextPos]) < 2) {
+              return $this->_response("Invalid parameter(s)", 400);
+            }
+          }
+          break;
+          
+      }
+      if ((int)method_exists($this, $this->endpoint) > 0) {
+        $response = $this->{$this->endpoint}($this->args);
+        if ($response == 'success') {
+          return $this->_response("OK", 200);
+        }
+        else if ($response == 'null_results') {
+          return $this->_response("Not Found", 404);
+        } else  if ($response == 'database_error') {
+          return $this->_response("Internal Error", 500);
+        } else if ($response == 'duplicate_entry') {
+          return $this -> _response('Conflict', 409);
+        } else if ($response == 'unauthorized') {
+          return $this -> _response('Unauthorized', 401);
+        } else {
+          return $this->_response($response);
+        }
+      }
+      return $this->_response("No Endpoint: $this->endpoint", 404);
+    }
+
+    private function _response($data, $status = 200) {
+      header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
+      return json_encode($data, JSON_UNESCAPED_SLASHES);
+    }
+
+    private function _cleanInputs($data) {
+        $clean_input = Array();
+ 
+        // hack for jquery not passing DELETE params.
+        if (is_array($data)) {
+            foreach ($data as $k => $v) {
+                $clean_input[$k] = $this->_cleanInputs($v);
+            }
+        } else {
+            $clean_input = trim(strip_tags($data));
+        }
+        return $clean_input;
+    }
+
+    private function _requestStatus($code) {
+        $status = array(  
+            200 => 'OK',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            403 => 'Forbidden',
+            404 => 'Not Found',   
+            405 => 'Method Not Allowed',
+            409 => 'Conflict',
+            500 => 'Internal Server Error',
+        ); 
+        return ($status[$code])?$status[$code]:$status[500]; 
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=utf-8> 
+    <title>QUESTLOG</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript">
+      window.jQuery || document.write('<script type="text/javascript" src="js/plugins/jquery.min.js"><\/script>');
+    </script>
+    <script type="text/javascript" src="js/plugins/jquery.transit.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jquery.qtip.min.js"></script>
+    <!--
+    <script src="js/plugins/converse.min.js"></script>
+    -->
+    <script src="js/utils/generateNames.js"></script>
+    <script src="js/users.js"></script>
+    <script src="js/quests.js"></script>
+    <script src="js/posts.js"></script>
+    <script src="js/main.js"></script>
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/newQuest.css" type="text/css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/plugins/converse.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/plugins/jquery.qtip.min.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/vader/jquery-ui.css" />
+  </head>
+  <body>
+    <div class="wrapper">
+      <header class="floatLeft" id="leftHeader">
+        <img src="img/title.05.gif" alt="QuestLog" id="titleImg"><br />
+        <div id="warningMessage">This is QuestLog. RPGs and collaborative storytelling.<br>
+        <span style="color:#fff;font-size:14px;">Current Status: <b>CLOSED BETA</b></span>
+      </header>
+      <hr class="clear"></hr>
+      <div id="mainContent"></div>
+      <hr></hr>
+      <div class="push"></div>
+    </div>
+  <footer></footer>
+  </body>
+</html>
